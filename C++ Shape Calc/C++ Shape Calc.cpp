9159399b214ee25317105bc88898705a95b22a22
Joseph Cuesta Acevedo <N1001723@my.ntu.ac.uk>
2022-01-24 00:28:10 +0000

    Created a main menu system and Int validation system and incorporated it into Main

--- C++ Shape Calc/C++ Shape Calc.cpp
+++ C++ Shape Calc/C++ Shape Calc.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main()
-{
-    cout << "Welcome to the shape calculator: \n" << endl;
-}
+class Validation {
+public:
+	int intValidation(string inputMessage) {
+		bool loopControl = true;
+		string rawValue;
+		int convertedValue;
+		while (loopControl)
+		{
+			cout << inputMessage << endl;
+			cin >> rawValue;
+			try
+			{
+				convertedValue = stoi(rawValue);
+				loopControl = false;
+				return convertedValue;
+			}
+			catch (const std::exception&)
+			{
+				cout << "Value " << rawValue << " could not be converted to INT, Try again" << endl;
+			}
 
+		}
+	}
+};
+
+class MainMenu {
+public:
+	Validation val;
+	string shapes[6] = { "Sqaure", "Rectangle", "Circle", "Cube", "Cuboid", "Sphere" };
+	void mainMenu() {
+		int menuChoice;
+		cout << "Welcome to the Shape Calculator" << endl;
+		cout << "Please select what shape you would like to calculate" << endl;
+		menuChoice = val.intValidation("Select a Shape from the Menu: 1.Sqaure 2.Rectangle 3.Circle 4.Cube 5.Cuboid 6.Sphere");
+	}
+};
+
+int main(){
+	MainMenu MM;
+	MM.mainMenu();
+}
